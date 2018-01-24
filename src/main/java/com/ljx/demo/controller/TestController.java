ef217cceb547381cc7ca279798614344cdd9590b
2394972032 <2394972032@qq.com>
2018-01-24 14:38:44 +0800

    update

--- src/main/java/com/ljx/demo/controller/TestController.java
+++ src/main/java/com/ljx/demo/controller/TestController.java
@@ -4,10 +4,13 @@ import com.ljx.demo.model.User;
 import com.ljx.demo.response.ResponseEntity;
 import com.ljx.demo.response.ResponseMoreEntity;
 import com.ljx.demo.utils.MakeOrderNum;
+import com.ljx.demo.utils.wechatUtils.WxConfigUtil;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 @RestController
@@ -48,4 +51,18 @@ public class TestController {
         return ResponseEntity.SUCC().data(MakeOrderNum.makeOrderNum()).build();
     }
 
+    @RequestMapping(value = "/share", method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String, Object> share(HttpServletRequest request) {
+        String urlTemp = "http://" + request.getServerName() + request.getContextPath();
+        String urlpath = "http://" + request.getServerName();
+        String appUrl = request.getParameter("url");
+        if (request.getParameter("code") != null) {
+            appUrl += "&code=" + request.getParameter("code");
+        }
+        if (request.getParameter("state") != null) {
+            appUrl += "&state=" + request.getParameter("state");
+        }
+        return WxConfigUtil.getSignature(appUrl, "自己的appid", "自己的secret", urlTemp, urlpath);
+    }
 }
